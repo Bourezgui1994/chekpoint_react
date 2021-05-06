744799675455f2984e9ef27c8facd31519d2ac0e
Amal Bourezgui <bouregui.ing1994@gmail.com>
2021-05-06 17:42:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import image from './img1.jpg';
 import './App.css';
+import './style.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+
+              <h1 className="title red"> Hello I'am Amal Bourezgui </h1>
+              <br/>
+
+              <div className="intro"> 
+                  <img src={image} alt='imageProfile ' className='imgProfile'/>
+                  <h2>I'am a developper web</h2>
+              </div>
+
+              <br/>
+              <img src="/img3.jpeg" className='img3' />
+        </div>
+        
+        <contrôles de la largeur vidéo={320} height={240}>
+          <source src="myVideo.mp4" type="video / mp4" />
+        </contrôles>
     </div>
   );
 }
